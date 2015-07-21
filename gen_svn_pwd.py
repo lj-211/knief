ce0dc6504193dbb4c72664874db9ce2bab6cb591
lj <lj@localhost.localdomain>
2015-07-21 09:05:46 +0800

    [mod] 1.完善生成密码工具

--- gen_svn_pwd.py
+++ gen_svn_pwd.py
@@ -23,6 +23,19 @@ if __name__ == "__main__":
 
 	common.com_utils.print_red("用户名为: " + user_name)
 	pwd = ""
+	small = ord('a') - 1
+	big = ord('A') - 1
+	bigger = small
+	smaller = big
+	if big > bigger:
+		bigger = big
+		smaller = small	
+	common.com_utils.print_notice("大写: " + str(bigger) + " 小写: " + str(smaller))
+	#user_name = "abcdefghijklmnopqrst"
 	for c in user_name:
-		pwd += str(ord(c))
+		to_int = ord(c)
+		if to_int >= bigger:
+			pwd += str(to_int - bigger)
+		else:
+			pwd += str(to_int - smaller)
 	common.com_utils.print_notice("生成密码为: " + pwd);
